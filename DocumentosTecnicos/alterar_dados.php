88ec9a803a9b6b90f6ff9ebf70c6a8c05399e164
noemis13 <noemischerer13@gmail.com>
2016-11-23 21:07:04 -0200

    Alterar dados

--- DocumentosTecnicos/alterar_dados.php
+++ DocumentosTecnicos/alterar_dados.php
@@ -1,13 +1,8 @@
 <?php
 require_once("_con.php"); 
 
-$estado = _POST["unidade_federativa"];
-$endereco = _POST["endereco"];
-$login = _POST["login"];
-$senha = _POST["senha"];
 $dbc = mysqli_connect($host, $user, $pass, $db);
-$user = _GET["login"];
-echo("user");
+
 if (mysqli_connect_errno()) {
     printf("Connect failed: %s\n", mysqli_connect_error());
     exit();
@@ -15,11 +10,36 @@ if (mysqli_connect_errno()) {
 echo("Ok.");
 }
 
-$query = mysqli_query($dbc, "UPDATE estado, endereco, login, senha SET unidade_federativa = '$estado', endereço = '$endereco', login = '$login', senha = '$senha'");
+$sql = mysqli_query($dbc, "SELECT u.unidade_federativa, u.endereco, u.login, u.senha FROM usuario u");
+$query = mysqli_query($dbc, $sql);
+
+while($dados = $query->fetch_assoc()){
+	$estado = _POST["unidade_federativa"];
+	$endereco = _POST["endereco"];
+	$login = _POST["login"];
+	$senha = _POST["senha"];
+	
+}
+mysqli_close($dbc);
+/*
+$query = mysqli_query($dbc, "UPDATE usuario SET 
+			     unidade_federativa = '$estado', 
+			     endereço = '$endereco', 
+			     login = '$login', 
+			     senha = '$senha'");
 mysqli_query($dbc, $query);
 mysqli_close($dbc);
 
-
-header('Location: pagina_inicial.html');
+*/
+//header('Location: pagina_inicial.html');
 ?>
 
+<form id="form1" name="form1" method="post" action="salvar_alterar_dados.php">
+<input type="text" readonly name="id" id="id" value="<?php echo $estado;?>" /><br>
+<input type="text" name="nome" id="nome" value="<?php echo $endereco;?>" /><br>
+<input type="text" name="email" id="email" value="<?php echo $login;?>" /><br>
+<input type="text" readonly name="data" id="data" value="<?php echo $senha;?>" /><br>
+<input type="submit" onClick="return confirm('Deseja atualizar o registro?');" name="Submit" value="SALVAR ALTERAÇÕES" id="button-form" />
+ </form>
+ 
+</div></div>
